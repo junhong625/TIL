dcba6152711e1c1b57ceb42138002914a3e2128e
Junhong Ahn <junhong625@naver.com>
2022-07-25 22:23:38 +0900

    LeetCode 문제 풀이

---
+++ Algorithm/LeetCode/[LeetCode 1번] Two Sum.py
@@ -0,0 +1,9 @@
+# 브루트 포스 풀이
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        # 이중 for문을 통해 두 수의 합을 구하여 target과 비교
+        for idx1 in range(len(nums)):
+            for idx2 in range(idx1+1, len(nums)):
+                if nums[idx1] + nums[idx2] == target:
+                    return [idx1, idx2]
