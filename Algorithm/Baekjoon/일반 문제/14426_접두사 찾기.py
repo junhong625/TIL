8e882e0e223856eb0baa420a3e18b21eb3b68eaa
junhong625 <junhong625@naver.com>
2023-02-28 15:18:24 +0900

    TIL (2023/02/28) Update:

--- Algorithm/Baekjoon/일반 문제/14426_접두사 찾기.py
+++ Algorithm/Baekjoon/일반 문제/14426_접두사 찾기.py
@@ -1,27 +1,33 @@
 import sys
 
-N, M = map(int, sys.stdin.readline().strip().split())
+input = sys.stdin.readline
 
-temp = []                                       # 문자열 집합
-for i in range(N):
-    temp.append(sys.stdin.readline().strip())
+N, M = map(int, input().split())
 
-find = []                                       # 접두사인지 확인할 문자열
-for i in range(M):
-    find.append(sys.stdin.readline().strip())
+def check(N, M):
+    words = {}
 
-temp.sort()                                     # 두 리스트 모두 정렬 
-find.sort()
+    for _ in range(N):
+        word = input().rstrip()
+        dummy = words
+        for char in word:
+            if dummy.get(char):
+                dummy = dummy[char]
+            else:
+                dummy[char] = {"is" : True}
+                dummy = dummy[char]
 
+    ans = 0
+    for _ in range(M):
+        word = input().rstrip()
+        dummy = words
+        for idx, char in enumerate(word):
+            if dummy.get(char):
+                dummy = dummy[char]
+            else:
+                break
+        else:
+            ans += 1
 
-cnt = 0
-idx = 0
-
-for i in find:                                  
-    for j in range(idx, N):                     # 같은 문자열이 여러번 주어지는 경우가 없고 정렬을 했기 때문에 탐색의 기준점을 idx로 지정해도 무방
-        if temp[j].startswith(i):               # temp[j]의 문자열이 i로 시작하는지 판단
-            cnt += 1                            # 접두사일 경우 cnt + 1
-            idx = j                             # idx 변경
-            break
-
-print(cnt)
+    print(ans)
+check(N, M)
