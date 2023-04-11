bc7ddfdbf0eff50e92e9f95fba92f799a522063c
안준홍 <junhong625@naver.com>
2023-04-11 15:52:49 +0900

    TIL (2023/04/11) Update

--- Algorithm/Baekjoon/Gold/[9084번] 동전.py
+++ Algorithm/Baekjoon/Gold/[9084번] 동전.py
@@ -1,28 +1,16 @@
 import sys
-from collections import deque
-import datetime
 input = sys.stdin.readline
 
-def bfs(target):
-    q = deque([target])
-    cnt = 0
-    idx = 0
-    while q and idx < len(coins):
-        print(q)
-        t = q.popleft()
-        if t <= 0:
-            if t == 0: cnt += 1
-            continue
-        cost = coins[idx]
-        for i in range(1, t//cost+1):
-            q.append(t-cost*i) 
-        idx += 1
-    return cnt
-
-T = int(input())
-for _ in range(T):
-    N = int(input())
-    coins = list(map(int, input().split()))
-    target = int(input())
-    print(bfs(target))
-print(f'ab{a}')
+def cal(target, coins):
+    dp = [1] + [0 for _ in range(target)]
+    for coin in coins:
+        for i in range(coin, target+1):
+            if i - coin >= 0:
+                dp[i] += dp[i-coin]
+    print(dp[target])
+    
+for _ in range(int(input().strip())):
+    N = int(input().strip())
+    coins = list(map(int, input().strip().split()))
+    target = int(input().strip())
+    cal(target, coins)
